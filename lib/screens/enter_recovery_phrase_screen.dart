859373020729a76cb2c4fbe2ac1367f691b4ee2f
Szczepański Łukasz <l.szczepanski@etogruppe.com>
2025-07-07 16:01:15 +0200

    make screen scrollable (auto height)

--- lib/screens/enter_recovery_phrase_screen.dart
+++ lib/screens/enter_recovery_phrase_screen.dart
@@ -28,49 +28,56 @@ class _EnterRecoveryPhraseScreen extends State<EnterRecoveryPhraseScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Padding(
-      padding: const EdgeInsets.all(16),
-      child: GestureDetector(
-        behavior: HitTestBehavior.translucent,
-        onTap: () {
-          _tapNotifier.value = true;
-        },
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          TextButton(
-            style: TextButton.styleFrom(foregroundColor: Colors.black),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
+      resizeToAvoidBottomInset: true,
+      body: SafeArea(
+        child: GestureDetector(
+          behavior: HitTestBehavior.translucent,
+          onTap: () {
+            _tapNotifier.value = true;
+          },
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Icon(Icons.arrow_back),
-                SizedBox(width: 8),
-                Text('Back'),
+                TextButton(
+                  style: TextButton.styleFrom(foregroundColor: Colors.black),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: const [
+                      Icon(Icons.arrow_back),
+                      SizedBox(width: 8),
+                      Text('Back'),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 18),
+                Text(
+                  'Enter your recovery phrase',
+                  style: AppTextStyles.boldCentered,
+                ),
+                const SizedBox(height: 8),
+                const Text(
+                  'Please enter your recovery phrase. It is 24 words long, all lowercase, with spaces.',
+                  style: TextStyle(color: AppColors.subtext),
+                ),
+                const SizedBox(height: 24),
+                const Text(
+                  'Enter your secret recovery phrase',
+                  style: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 20),
+                MnemonicInput(tapNotifier: _tapNotifier, wordList: WORDLIST),
+                const SizedBox(height: 40),
               ],
             ),
           ),
-          const SizedBox(height: 18),
-          Text('Enter your recovery phrase', style: AppTextStyles.boldCentered),
-          const SizedBox(height: 8),
-          const Text(
-            'Please enter your recovery phrase is 24 words long, all lower case, with spaces.',
-            style: TextStyle(color: AppColors.subtext),
-          ),
-          const SizedBox(height: 24),
-          const Text(
-            'Enter your secret recovery phrase',
-            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsets.all(0.0),
-            child: SingleChildScrollView(
-              child: MnemonicInput(tapNotifier: _tapNotifier, wordList: WORDLIST),
-            ),
-          )
-        ]),
+        ),
       ),
-    ));
+    );
   }
 }
