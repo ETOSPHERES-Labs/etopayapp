fca68b929469df0c0e23caed9cb284d39c8397ff
Szczepański Łukasz <l.szczepanski@etogruppe.com>
2025-07-16 09:35:31 +0200

    fix Continue button

--- lib/screens/choose_custom_network_screen.dart
+++ lib/screens/choose_custom_network_screen.dart
@@ -17,14 +17,14 @@ class _ChooseCustomNetworkScreen extends State<ChooseCustomNetworkScreen> {
       'coin': 'ETH',
       'node': 'https://mainnet.infura.io/v3/...',
       'username': 'eth_user',
-      'something': 'eth_pass'
+      'something': 'eth_brrr'
     },
     {
       'name': 'Bitcoin',
       'coin': 'BTC',
       'node': 'https://btcnode.org',
       'username': 'btc_user',
-      'something': 'btc_pass'
+      'something': 'btc_brrr'
     },
     {
       'name': 'Polygon',
@@ -41,8 +41,9 @@ class _ChooseCustomNetworkScreen extends State<ChooseCustomNetworkScreen> {
   final TextEditingController nodeController = TextEditingController();
   final TextEditingController usernameController = TextEditingController();
   final TextEditingController somethingController = TextEditingController();
-  
+
   bool _isContinueButtonEnabled = false;
+  static const double _continueButtonHeight = 64;
 
   void _setContinueButtonState(bool isValid) {
     setState(() {
@@ -119,89 +120,98 @@ class _ChooseCustomNetworkScreen extends State<ChooseCustomNetworkScreen> {
     final bool isNetworkSelected = selectedNetwork != null;
 
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
-        child: LayoutBuilder(
-          builder: (context, constraints) {
-            return SingleChildScrollView(
-              padding: const EdgeInsets.all(16.0),
-              child: ConstrainedBox(
-                constraints: BoxConstraints(minHeight: constraints.maxHeight),
-                child: IntrinsicHeight(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      TextButton(
-                        style:
-                            TextButton.styleFrom(foregroundColor: Colors.black),
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: const Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Icon(Icons.arrow_back),
-                            SizedBox(width: 8),
-                            Text('Back'),
-                          ],
-                        ),
+        top: true,
+        bottom: true,
+        child: Column(
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TextButton(
+                      style:
+                          TextButton.styleFrom(foregroundColor: Colors.black),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(Icons.arrow_back),
+                          SizedBox(width: 8),
+                          Text('Back'),
+                        ],
                       ),
-                      Expanded(
-                        child: ImageCardListWidget(
-                          svgAssetPath: 'assets/images/choose_network_bg.svg',
-                          subtitle: 'Connect to a custom network',
-                          footer: ConditionalButton(
-                            isActive: _isContinueButtonEnabled,
-                            onPressed: () {},
-                            text: 'Continue',
+                    ),
+                    ImageCardListWidget(
+                      svgAssetPath: 'assets/images/choose_network_bg.svg',
+                      subtitle: 'Connect to a custom network',
+                      contentBeforeFooter: Column(
+                        children: [
+                          SizedBox(
+                            height: 53,
+                            child: DropdownButtonFormField<Map<String, String>>(
+                              decoration: activeDecoration()
+                                  .copyWith(labelText: 'Network'),
+                              value: selectedNetwork,
+                              style: textStyle(true),
+                              items: networks.map((network) {
+                                return DropdownMenuItem(
+                                  value: network,
+                                  child: Text(network['name']!),
+                                );
+                              }).toList(),
+                              onChanged: (network) {
+                                setState(() {
+                                  selectedNetwork = network;
+                                  if (network != null) {
+                                    _updateControllers(network);
+                                  }
+                                });
+                              },
+                            ),
                           ),
-                          contentBeforeFooter: Column(
-                            children: [
-                              SizedBox(
-                                height: 53,
-                                child: DropdownButtonFormField<
-                                    Map<String, String>>(
-                                  decoration: activeDecoration()
-                                      .copyWith(labelText: 'Network'),
-                                  value: selectedNetwork,
-                                  style: textStyle(true),
-                                  items: networks.map((network) {
-                                    return DropdownMenuItem(
-                                      value: network,
-                                      child: Text(network['name']!),
-                                    );
-                                  }).toList(),
-                                  onChanged: (network) {
-                                    setState(() {
-                                      selectedNetwork = network;
-                                      if (network != null) {
-                                        _updateControllers(network);
-                                      }
-                                    });
-                                  },
-                                ),
-                              ),
-                              const SizedBox(height: 12),
-                              _buildTextField('Coin type', coinController,
-                                  isNetworkSelected),
-                              const SizedBox(height: 12),
-                              _buildTextField('Node address', nodeController,
-                                  isNetworkSelected),
-                              const SizedBox(height: 12),
-                              _buildTextField('Username (optional)',
-                                  usernameController, isNetworkSelected),
-                              const SizedBox(height: 12),
-                              _buildTextField('something (optional)',
-                                  somethingController, isNetworkSelected),
-                            ],
-                          ),
-                        ),
+                          const SizedBox(height: 12),
+                          _buildTextField(
+                              'Coin type', coinController, isNetworkSelected),
+                          const SizedBox(height: 12),
+                          _buildTextField('Node address', nodeController,
+                              isNetworkSelected),
+                          const SizedBox(height: 12),
+                          _buildTextField('Username (optional)',
+                              usernameController, isNetworkSelected),
+                          const SizedBox(height: 12),
+                          _buildTextField('something (optional)',
+                              somethingController, isNetworkSelected),
+                          const SizedBox(height: 16),
+                        ],
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
-            );
-          },
+            ),
+            Container(
+              height: _continueButtonHeight,
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              color: Colors.white,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ConditionalButton(
+                    isActive: _isContinueButtonEnabled,
+                    onPressed: () {},
+                    text: 'Continue',
+                  )
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
