cbef8770770e9bbe49af6818990108e532a1ae40
Szczepański Łukasz <l.szczepanski@etogruppe.com>
2025-08-21 14:42:55 +0200

    fix curve and shadows in notched menu

--- lib/widgets/home_shell/rounded_notched_shape.dart
+++ lib/widgets/home_shell/rounded_notched_shape.dart
@@ -20,28 +20,28 @@ class RoundedNotchedShape extends NotchedShape {
     double startX = centerOfScreen - 2 * radius;
 
     double maximum = top;
-    double minimum = top + radius;
+    double minimum = top + radius + 10;
     double middle = minimum / 2;
 
     path.moveTo(left, maximum);
     path.lineTo(startX , maximum);
 
     double point1 = startX + radius;
-      path.arcToPoint(Offset(point1, middle + 4),
+      path.arcToPoint(Offset(point1+3.5, middle ),
       radius: Radius.circular(qradius),
         clockwise: true,
     );
 
     double point2 = point1 - 2 + 2 * radius;
     path.arcToPoint(
-      Offset(point2, middle + 4),
+      Offset(point2, middle),
       radius: Radius.circular(radius),
       clockwise: false,
     );
 
     double point3 = point2 + radius;
 
-    path.arcToPoint(Offset(point3, maximum),
+    path.arcToPoint(Offset(point3-3.5, maximum),
     radius: Radius.circular(qradius),
       clockwise: true,
     );
--- lib/widgets/home_shell/rounded_notched_shape_with_shadow_painter.dart
+++ lib/widgets/home_shell/rounded_notched_shape_with_shadow_painter.dart
@@ -12,7 +12,8 @@ class RoundedNotchedShapeWithShadowPainter extends CustomPainter {
     final notchedShape = RoundedNotchedShape();
     final path = notchedShape.getOuterPath(host, guest);
 
-    canvas.drawShadow(path, const Color.fromARGB(255, 44, 160, 255), 18.0, true);
+    canvas.drawShadow(path, const Color.fromARGB(255, 44, 160, 255), 20.0, true);
+    canvas.drawShadow(path, const Color.fromARGB(255, 44, 160, 255), 30.0, true);
 
     final paint = Paint()
       ..color = Colors.white
